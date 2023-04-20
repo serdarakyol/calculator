591e30d5e9c895217e260e5c271fb8ede734868f
Serdar AKYOL <serdarakyol55@outlook.com>
2023-04-20 10:43:14 +0200

    Add unit tests

--- src/main/java/com/serdar/calculator/entity/CustomRequest.java
+++ src/main/java/com/serdar/calculator/entity/CustomRequest.java
@@ -1,5 +1,12 @@
 package com.serdar.calculator.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class CustomRequest {
     public String firstNumber;
     public String secondNumber;
