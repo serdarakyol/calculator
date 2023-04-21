a874c8882d8f65da5a54a7234d148c96c46d379c
Serdar AKYOL <serdarakyol55@outlook.com>
2023-04-21 21:24:41 +0200

    Added more test (#12)
    * added test for CustomRequest
    
    * added test to controller
    
    * update readme

---
+++ src/test/java/com/serdar/calculator/entity/CustomRequestTest.java
@@ -0,0 +1,37 @@
+package com.serdar.calculator.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CustomRequestTest {
+
+    private CustomRequest customRequest;
+
+    @BeforeEach
+    void setUp() {
+        customRequest = new CustomRequest();
+        customRequest.setFirstNumber(new BigDecimal("3.123456789"));
+        customRequest.setSecondNumber(new BigDecimal("4.123456789"));
+        customRequest.setOperation('+');
+    }
+
+    @Test
+    void testCustomRequest() {
+        // Given
+        customRequest.setFirstNumber(new BigDecimal("5.123456789"));
+        customRequest.setSecondNumber(new BigDecimal("10.123456789"));
+        customRequest.setOperation('-');
+
+        // When, Then
+        assertEquals("5.123456789", customRequest.getFirstNumber().toString());
+        assertEquals("10.123456789", customRequest.getSecondNumber().toString());
+        assertEquals('-', customRequest.getOperation());
+        assertEquals(
+                "CustomRequest(firstNumber=5.123456789, secondNumber=10.123456789, operation=-)",
+                customRequest.toString());
+    }
+}
